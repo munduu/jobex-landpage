1564197ac1fd3872c7c70092bfc1b19dfa2f62a6
Aguiar <marcustsu@gmail.com>
2019-03-28 17:17:57 -0300

    home

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var Typeahead = require('typeahead');
+ 
+var input = document.createElement('input');
+ 
+// source is an array of items
+var ta = Typeahead(input, {
+    source: ['foo', 'bar', 'baz']
+});

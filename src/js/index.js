63c575444d62fe4e161e20d1e9eabae12395912f
Otavio <otaviollneto@gmail.com>
2019-04-05 17:16:19 -0300

    inclusão da busca

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,12 @@
 $(function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
 
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
+  //lista profissões
+  var availableTags = []
+  $.post("http://soujobex.com.br/servicosLand", function(result){
+    for(var i = 0; i < result.result.list.data.length; i++) {
+      availableTags[i] = result.result.list.data[i].nome;
+      $( "#tags" ).autocomplete({ source: availableTags });
+    }
   });
+  
+});

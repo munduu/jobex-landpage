664b09aab1f4be69222f2640841c779f99a08b58
Otavio <otaviollneto@gmail.com>
2019-04-24 10:53:52 -0300

    go

--- src/js/perfil.js
+++ src/js/perfil.js
@@ -44,12 +44,12 @@ $(function() {
           '<div id="img_perfil_detail'+ result.result.perfil[i].id_user +'" class="card-header"></div>'+
           '<div class="card-body">'+
             '<h5 class="card-title">'+ result.result.perfil[i].nome +'</h5>'+
-            '<p class="card-text">Telefone: ('+ result.result.perfil[i].area +') '+ result.result.perfil[i].telefone +'</p>'+
-            '<p class="card-text">E-mail: '+ result.result.perfil[i].email +'</p>'+
-            '<p class="card-text">Observação: '+ result.result.perfil[i].obs +'</p>'+
-            '<p class="card-text">Aceita Cartão: '+ aceita_cartao +'</p>'+
-            '<p class="card-text">Cobra Visita? '+ cobra_visita +'</p>'+
-            '<p class="card-text">Emite Nota? '+ mei +'</p>'+
+            '<p class="card-text"><strong>Telefone:</strong> ('+ result.result.perfil[i].area +') '+ result.result.perfil[i].telefone +'</p>'+
+            '<p class="card-text"><strong>E-mail:</strong> '+ result.result.perfil[i].email +'</p>'+
+            '<p class="card-text"><strong>Observação:</strong> '+ result.result.perfil[i].obs +'</p>'+
+            '<p class="card-text"><strong>Aceita Cartão:</strong> '+ aceita_cartao +'</p>'+
+            '<p class="card-text"><strong>Cobra Visita?</strong> '+ cobra_visita +'</p>'+
+            '<p class="card-text"><strong>Emite Nota?</strong> '+ mei +'</p>'+
           '</div>'+
           '<div class="card-footer text-muted">'+
             '<p style="text-align: center; margin: 5px;">'+

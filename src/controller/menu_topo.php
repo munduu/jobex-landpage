97e9b06308a3df176c4fe936ca1da9a70235ab10
Aguiar <marcustsu@gmail.com>
2019-04-25 16:52:43 -0300

    layout topo

--- src/controller/menu_topo.php
+++ src/controller/menu_topo.php
@@ -3,9 +3,8 @@
 <link href="<?php if ($pageName=='index'){ echo './src/css/menu-topo.css'; }else{ echo '../css/menu-topo.css';} ?>" rel="stylesheet">
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-center header darkTheme">
-    <a class="navbar-brand" href="<?php if ($pageName=='index'){ echo '#'; }else{ echo '../../';} ?>">
+    <a class="navbar-brand logoContainer text-left text-md-left text-lg-center" href="<?php if ($pageName=='index'){ echo '#'; }else{ echo '../../';} ?>">
       <div><img class="headerLogo" src="<?php if($pageName=='index') { echo 'img/icone_topo_branca.png'; } else {  echo '../../img/icone_topo_branca.png'; } ?>" /></div>
-
     </a>
     <button class="navbar-toggler" style="color: #ffffff;"type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,26 +13,26 @@
 
     <div class="collapse navbar-collapse menuContainer" id="navbarSupportedContent">
       <ul class="navbar-nav myMenu">
-        <li class="nav-item mx-auto">
+        <li class="nav-item mr-3">
           <a class="nav-link <?php if ($pageName=='index'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo '#'; }else{ echo '../../';} ?>">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item mx-auto">
+        <li class="nav-item mr-3">
           <a class="nav-link <?php if ($pageName=='sou-cliente'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo './sou-cliente'; }else{ echo '../../sou-cliente';} ?>">Sou Cliente</a>
         </li>
-        <li class="nav-item mx-auto">
+        <li class="nav-item mr-3">
           <a class="nav-link <?php if ($pageName=='sou-profissional'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo './sou-profissional'; }else{ echo '../../sou-profissional';} ?>">Sou Profissional</a>
         </li>
-        <li class="nav-item mx-auto">
+        <li class="nav-item mr-3">
           <a class="nav-link <?php if ($pageName=='quero-aprender'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo './quero-aprender'; }else{ echo '../../quero-aprender';} ?>">Quero Aprender</a>
         </li>
-        <li class="nav-item mx-auto">
+        <li class="nav-item mr-3">
 
           <a class="nav-link <?php if ($pageName=='blog'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo './blog'; }else{ echo '../../blog';} ?>">Blog</a>
         </li>
         <li class="nav-item mr-3">
           <a class="nav-link <?php if ($pageName=='app'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo './app'; }else{ echo '../../app';} ?>"><strong>APP Jobex</strong></a>
         </li>
-        <li class="nav-item mx-auto">
+        <li class="nav-item">
           <a class="nav-link myAccountButton <?php if ($pageName=='login'){ echo 'activeHeader'; }?>" href="<?php if ($pageName=='index'){ echo './login'; }else{ echo '../../login';} ?>" style="color: #009DBE;">Minha Conta <i
 
               class="far fa-user"></i></a>
